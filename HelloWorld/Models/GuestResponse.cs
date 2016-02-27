0680a9750960429fcfeb7ff39d9b98f1faaf82db
Dan <danv@cstructor.com>
2016-02-27 11:14:51 -0800

    bootstrap exercises

--- HelloWorld/Models/GuestResponse.cs
+++ HelloWorld/Models/GuestResponse.cs
@@ -10,8 +10,16 @@ namespace HelloWorld.Models
     {
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
+        [Phone]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage="Please enter a valid email")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter if you will attend")]
         public bool? WillAttend { get; set; }
     }
 }
