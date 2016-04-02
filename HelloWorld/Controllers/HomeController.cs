6f2ba8c40e163bc9175a4a03e69be84c9e115f1d
Dan <danv@cstructor.com>
2016-04-02 11:43:26 -0700

    add userRepository

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -14,10 +14,47 @@ namespace HelloWorld.Controllers
     public class HomeController : Controller
     {
         private IProductRepository productRepository;
+        private IUserRepository userRepository;
 
-        public HomeController(IProductRepository productRepository)
+        public HomeController(IProductRepository productRepository, IUserRepository userRepository)
         {
             this.productRepository = productRepository;
+            this.userRepository = userRepository;
+        }
+
+        [Authorize]
+        public ActionResult Notes()
+        {
+            return View();
+        }
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userRepository.LogIn(model.UserName, model.Password);
+                if (user != null)
+                {
+                    System.Web.Security.FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    return Redirect(returnUrl);
+                }
+
+                ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+            return Redirect("~/");
         }
 
         public ActionResult SetCookie()
